8e12fee635608ff3683a69b7f02c0fd303cd4ce1
JJMPWizard <jjmendez@oasishoteles.com>
2022-09-04 14:23:46 -0500

    version completed

--- requirements.sh
+++ requirements.sh
@@ -1,24 +1,90 @@
 #!/bin/bash
-# @argument mac or linux only
-SYSTEM=$1
+###################################################################################
+# name: Requirements Script
+# version: 1.0.1
+# description: This script install all dependecies in the requirements file
+# keywords: Bash, Dependecies
+# author: JJWizardMP
+# lastupdate: 15/05/2022
+###################################################################################
+# arguments:
+# @SYSTEM:
+#   mac: Mac OS
+#   debian: Ubuntu/Debian
+#   rhl(Red Hat Linux Original): RHEL / CentOS / Fedora (yum)
+#   open: OpenSUSE
+#   arch: ArchLinux
+###################################################################################
+declare -A ERRORS_MSGS=( [SYSTEM]="Pass the correct argument for select \
+the operating system:\n-debian\n-rhl\n-open\n-arch\n-mac" )
+# Handle enviroment variables
+setEnvVars(){
+    export SYSTEM=$1
+}
+unsetEnvVars(){
+    unset SYSTEM
+}
+# Handle argument error
+throwError_wrongSystem(){
+    echo -e ${ERRORS_MSGS[SYSTEM]}
+    exit 1
+}
+# Update dependecies before install requirements
+updateSystemDependecies(){
+    case $SYSTEM in
+        debian)
+            sudo apt update -y
+        ;;
+        rhl)
+            sudo yum update -y
+        ;;
+        open)
+            sudo zypper update -y
+        ;;
+        arch)
+            sudo pacman -Sy
+        ;;
+        mac)
+            sudo brew update -y
+        ;;
+        *)
+            throwError_wrongSystem
+        ;;
+    esac
+}
+# Install Dependecies for project
 installDependecies(){
     while read -r line
     do
         case $SYSTEM in
-            linux)
-                sudo apt install "$line"
+            debian)
+                sudo apt install "$line" -y
+            ;;
+            rhl)
+                sudo yum install "$line" -y
+            ;;
+            open)
+                sudo zypper install "$line" -y
+            ;;
+            arch)
+                sudo pacman -Sy install "$line"
             ;;
             mac)
-                sudo brew install "$line"
+                sudo brew install "$line" -y
             ;;
             *)
-                echo "Pass \"mac\" or \"linux\" for select the operating system"
-                exit 1
+                throwError_wrongSystem
             ;;
         esac
     done < requirements
 }
-
-installDependecies
-
+# Main Function
+main(){
+    setEnvVars $1
+    updateSystemDependecies
+    installDependecies
+    unsetEnvVars
+}
+# Run main function
+main $1
 exit 0
--- script.sh
+++ script.sh
@@ -1,13 +1,64 @@
 #!/bin/bash
-###############################################################
+###################################################################################
+# name: Git/Github Switcher
+# version: 1.0.1
+# description: This script allow you change in differents git and github accounts,
+#               inclusive you log into your default browser
+# keywords: Bash, Git, Github, JQ, gh
+# author: JJWizardMP
+# lastupdate: 22/05/2022
+###################################################################################
 # @params
-# command: may be "github", "git" or "both" (for git and github account)
+# option: may be "github", "git" or "both" (for git and github account)
 # account: label for a git account
 # method: may be "token" or "browser" (Optional argument)
-###############################################################
-cmd=$1
-acc=$2
-med=$3
+###################################################################################
+declare -A ERRORS_MSGS=( \
+    [TWO_ARGS_REQUIRED]='Pass the two require arguments!\n' \
+    [FIRST_ARG_WRONG]='The first argument must be \"git\", \"github\" or \"both\"!\n' \
+    [SECOND_ARG_WRONG]='Account is not defined in config.json file!\n'
+    [THIRD_ARG_REQUIRED]='Pass the third arguments if you \
+    use \"both\"  or \"github\" login!\n' \
+    [THIRD_ARG_WRONG]='Pass \"token\" or \"browser\" as the third arguments \
+    for select the mode of authentication' \
+)
+
+declare -A STATE_MSGS=( \
+    [CURRENT_C]='Current credentials ...\n' \
+    [CURRENT_S]='Current status ...\n' \
+    [CURRENT_CS]='Current credentials and status...\n' \
+    [SETTING_C]='Setting credentials ...\n' \
+    [LOGIN]='Log In ...\n'
+    [NEW_C]='New credentials ...\n' \
+    [NEW_S]='New status ...\n' \
+    [DOTS]='"\n........................................\n"' \
+    [COMPLETED]='...........................\nCompleted!\n' \
+)
+
+declare -A G_MSGS=( \
+    [GIT]='Git: \n' \
+    [GITHUB]='Github: \n' \
+)
+setEnvVars(){
+    export option=$1
+    export account=$2
+    export method=$3
+}
+unsetEnvVars(){
+    unset option
+    unset account
+    unset method
+    unset user
+    unset email
+    unset token
+    unset ERRORS_MSGS
+    unset STATE_MSGS
+    unset G_MSGS
+}
+resetBash(){
+    exec bash
+    su - $(whoami)
+}
 # Show git credentials
 showGitCredentials(){
     git config --list --show-origin
@@ -16,27 +67,33 @@ showGitCredentials(){
 showGithubStatus(){
     gh auth status
 }
-# delete file with the github token
+# delete file with the github tokenen
 deleteToken(){
-    rm -rf /tmp/tokengithub
+    if [[ -e /tmp/tokengithub ]]
+    then
+        rm /tmp/tokengithub
+    fi
 }
-# Extract git credentials from a json file with the command jq (for more read the doc for jq)
+# Extract git credentials from a json file with the option jq
+# (for more read the doc for jq)
 setVariables(){
-    usr=$( jq ".$acc.user" config.json | tr -d '"' )
-    eml=$( jq ".$acc.email" config.json | tr -d '"' )
-    tok=$( jq ".$acc.token" config.json | tr -d '"' )
-    echo $tok > /tmp/tokengithub
+    export user=$( jq ".$account.user" config.json | tr -d '"' )
+    export email=$( jq ".$account.email" config.json | tr -d '"' )
+    export token=$( jq ".$account.token" config.json | tr -d '"' )
+    echo $token > /tmp/tokengithub
 }
-# Change git credentials from a json file with the command jq (for more read the doc for jq)
+# Change git credentials from a json file with the option jq
+# (for more read the doc for jq)
 changeGitCredentials() {
-    git config --global user.name "$usr"
-    git config --global user.email "$eml"
-    git config --global user.mail "$eml"
+    git config --global user.name "$user"
+    git config --global user.email "$email"
+    git config --global user.mail "$email"
 }
-# Change github credentials from a json file with the command jq (for more read the doc for jq)
+# Change github credentials from a json file with the option jq
+# (for more read the doc for jq)
 changeGithubCredentials(){
     gh auth logout
-    case $med in
+    case $method in
         token)
             gh auth login --with-token < /tmp/tokengithub
         ;;
@@ -44,80 +101,106 @@ changeGithubCredentials(){
             gh auth login --web
         ;;
         *)
-            echo "Pass \"token\" or \"browser\" for select the mode of authentication"
-            exit 1
+            throwError_thirdArgWrong
         ;;
     esac
 }
 verifyArguments(){
-    if [[ -z $cmd ]] || [[ -z $acc ]]
+    throwError_twoArgsRequired
+    throwError_secondArgWrong
+    throwError_thirdArgRequired
+}
+throwError_twoArgsRequired(){
+    if [[ -z $option ]] || [[ -z $account ]]
     then
-        echo -e "\nPass the two require arguments!\n"
+        echo -e ${ERRORS_MSGS[TWO_ARGS_REQUIRED]}
         exit 1
     fi
-    if [[ $cmd = "both" ]] && [[ -z $med ]]
+}
+throwError_thirdArgRequired(){
+    if ( [[ $option = "both" ]] || [[ $option = "github" ]] ) && [[ -z $method ]]
     then
-        echo -e "\nPass the third arguments if you use \"both\" login!\n"
+        echo -e ${ERRORS_MSGS[THIRD_ARG_REQUIRED]}
         exit 1
     fi
+}
+throwError_firstArgWrong(){
+    echo -e ${ERRORS_MSGS[FIRST_ARG_WRONG]}
+    exit 1
+}
+throwError_secondArgWrong(){
+    testaccount=$( jq ".$account.user" config.json )
+    if [[ $testaccount == "null" ]]
+    then
+        echo -e ${ERRORS_MSGS[SECOND_ARG_WRONG]}
+        exit 1
+    fi
+}
+throwError_thirdArgWrong(){
+    echo -e ${ERRORS_MSGS[THIRD_ARG_WRONG]}
+    exit 1
 }
 # Main script
 main(){
+    setEnvVars $1 $2 $3
     verifyArguments
     setVariables
-    case $cmd in
+    case $option in
         git)
-            echo "Current credentials ..."
-            echo -e "Git:\n"
+            echo -e ${STATE_MSGS[CURRENT_C]}
+            echo -e ${G_MSGS[GIT]}
             showGitCredentials
-            echo -e "\n..........\n"
-            echo "Setting credentials ..."
+            echo -e ${STATE_MSGS[DOTS]}
+            echo -e ${STATE_MSGS[SETTING_C]}
             changeGitCredentials
-            echo -e "\n..........\n"
-            echo "New credentials ..."
-            echo -e "Git:\n"
+            echo -e ${STATE_MSGS[DOTS]}
+            echo -e ${STATE_MSGS[NEW_C]}
+            echo -e ${G_MSGS[GIT]}
             showGitCredentials
-            echo -e "..........\nCompleted!"
+            echo -e ${STATE_MSGS[COMPLETED]}
         ;;
         github)
-            echo "Current status ..."
-            echo -e "Github:\n"
+            echo -e ${STATE_MSGS[CURRENT_S]}
+            echo -e ${G_MSGS[GITHUB]}
             showGithubStatus
-            echo -e "\n..........\n"
-            echo "Setting credentials ..."
+            echo -e ${STATE_MSGS[DOTS]}
+            echo -e ${STATE_MSGS[LOGIN]}
             changeGithubCredentials
-            echo -e "\n..........\n"
-            echo "New status ..."
-            echo -e "Github:\n"
+            echo -e ${STATE_MSGS[DOTS]}
+            echo -e ${STATE_MSGS[NEW_S]}
+            echo -e ${G_MSGS[GITHUB]}
             showGithubStatus
-            echo -e "..........\nCompleted!"
+            echo -e ${STATE_MSGS[COMPLETED]}
         ;;
         both)
-            echo "Current credentials and status ..."
-            echo -e "Git:\n"
+            echo -e ${STATE_MSGS[CURRENT_CS]}
+            echo -e ${G_MSGS[GIT]}
             showGitCredentials
-            echo -e "Github:\n"
+            echo -e ${G_MSGS[GITHUB]}
             showGithubStatus
-            echo -e "\n..........\n"
-            echo "Setting credentials ..."
+            echo -e ${STATE_MSGS[DOTS]}
+            echo -e ${STATE_MSGS[SETTING_C]}
             changeGitCredentials
+            echo -e ${STATE_MSGS[LOGIN]}
             changeGithubCredentials
-            echo -e "\n..........\n"
-            echo "New credentials ..."
-            echo -e "Git:\n"
+            echo -e ${STATE_MSGS[DOTS]}
+            echo -e ${STATE_MSGS[NEW_C]}
+            echo -e ${G_MSGS[GIT]}
             showGitCredentials
-            echo -e "Github:\n"
+            echo -e ${STATE_MSGS[NEW_S]}
+            echo -e ${G_MSGS[GITHUB]}
             showGithubStatus
-            echo -e "..........\nCompleted!"
+            echo -e ${STATE_MSGS[COMPLETED]}
         ;;
         *)
-            echo "Pass \"token\" or \"browser\" for select the mode of authentication"
-            exit 1
+            throwError_firstArgWrong
         ;;
     esac
     deleteToken
+    unsetEnvVars
+    resetBash
 }
 
-main
+main $1 $2 $3
 
 exit 0
